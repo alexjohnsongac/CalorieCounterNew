e343c017357f80e52b3641951f3cc5872508f7b0
alexjohnsongac <alexjohnson@gustavus.edu>
2025-03-19 13:52:08 -0500

    Google authentication error fixes

--- app/src/main/java/com/example/caloriecounternew/GoogleAuthClient.kt
+++ app/src/main/java/com/example/caloriecounternew/GoogleAuthClient.kt
@@ -1,5 +1,6 @@
 package com.example.caloriecounternew
 
+import android.app.Activity
 import android.content.Context
 import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
