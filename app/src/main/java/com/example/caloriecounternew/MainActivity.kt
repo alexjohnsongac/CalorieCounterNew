f5d3c056e55b25c821e687dcee7477396f68124b
alexjohnsongac <alexjohnson@gustavus.edu>
2025-04-02 15:43:17 -0500

    Getting pixel art to display correct, fixing bugs with the buttons, adjusting pixel art scales, adjust button positions.
    Need to do: make pixel art views dependent on streak.

--- app/src/main/java/com/example/caloriecounternew/MainActivity.kt
+++ app/src/main/java/com/example/caloriecounternew/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.caloriecounternew
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
@@ -21,10 +22,17 @@ class MainActivity : ComponentActivity() {
         val signOutButton: Button = findViewById(R.id.buttonSignOut)
         val goToFoodPageButton: Button = findViewById(R.id.buttonGoToFoodPage)
 
+        val eggView: ImageView = findViewById(R.id.pixelEggView)
+        val firstView: ImageView = findViewById(R.id.pixelFirstView)
+        val secondView: ImageView = findViewById(R.id.pixelSecondView)
+
+
         // Check if the user is signed in
         if (googleAuthClient.isSingedIn()) {
             signInButton.visibility = Button.GONE
             signOutButton.visibility = Button.VISIBLE
+            goToFoodPageButton.visibility = Button.VISIBLE
+            eggView.visibility = ImageView.VISIBLE
         } else {
             signInButton.visibility = Button.VISIBLE
             signOutButton.visibility = Button.GONE
@@ -38,6 +46,8 @@ class MainActivity : ComponentActivity() {
                 if(googleAuthClient.isSingedIn()) {
                     signInButton.visibility = Button.GONE
                     signOutButton.visibility = Button.VISIBLE
+                    goToFoodPageButton.visibility = Button.VISIBLE
+                    eggView.visibility = ImageView.VISIBLE
                 }
             }
         }
@@ -49,6 +59,8 @@ class MainActivity : ComponentActivity() {
                 googleAuthClient.signOut()
                 signInButton.visibility = Button.VISIBLE
                 signOutButton.visibility = Button.GONE
+                goToFoodPageButton.visibility = Button.GONE
+                eggView.visibility = ImageView.GONE
             }
         }
 
